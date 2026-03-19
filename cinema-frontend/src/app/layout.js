52c3479adf267cf82ee192c12ad1ed7e46f8079d
htetyeeaung37 <htetyeeaung5566@gmail.com>
2026-03-19 19:57:18 +0630

    change ui design

--- cinema-frontend/src/app/layout.js
+++ cinema-frontend/src/app/layout.js
@@ -3,6 +3,14 @@ import "./globals.css";
 import { AuthProvider } from "@/components/auth/AuthContext";
 import Footer from "@/components/layout/Footer/Footer";
 
+export const metadata = {
+  title: "JCGV Cinema | Book Your Favorite Movies Online",
+  description: "Explore the latest movies and showtimes at JCGV Cinema.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
